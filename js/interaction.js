3bd18547562a697d904c85b9163ca1c03b7393e9
Hyeon Seong LEE <dynamic1117@gmail.com>
2021-06-17 04:56:04 +0900

    code implement
    Save frequently used DOMs as variables

--- js/interaction.js
+++ js/interaction.js
@@ -1,33 +1,45 @@
-$(function(){
-    $('.slider_handler a').click(function(){
-        $(this).parent().siblings().removeClass('on');
-        $(this).parent().addClass('on');
-        $('.slider_handler span').css({
-            opacity:0
-        })
-        $(this).parent().siblings().animate({ //li
-            width: '5%'
-        },300)
-        $(this).parent().siblings().children().animate({ //a
-            width:'100%'
-        },0)
-        $(this).parent().animate({ //li
-            width:'95%'
-        },300)
-        $(this).animate({ //a
-            width:'100%'
-        },0)
-        return false;
+$(function () {
+  var $slider = $(".slider_handler");
+  $slider.find("a").click(function () {
+    $slider.find("li").removeClass("on");
+    $(this).parent().addClass("on");
+    $slider.find("span").css({
+      opacity: 0,
+    });
+    $(this).parent().siblings().animate(
+      {
+        //li
+        width: "5%",
+      },
+      300
+    );
+    $(this).parent().animate(
+      {
+        //li
+        width: "95%",
+      },
+      300
+    );
+    return false;
+  });
+  $slider
+    .find("span")
+    .mouseenter(function () {
+      var toggleCheck = $(this).parent().parent().hasClass("on");
+      if (toggleCheck == false)
+        $(this).animate(
+          {
+            opacity: 0.3,
+          },
+          100
+        );
     })
-    $('.slider_handler span').mouseenter(function(){
-        var toggleCheck = $(this).parent().parent().hasClass('on')
-        if (toggleCheck==false)
-        $(this).animate({
-            opacity:0.3
-        },100)
-    }).mouseleave(function(){
-        $(this).animate({
-            opacity:0
-        },100)
-    })
-})
+    .mouseleave(function () {
+      $(this).animate(
+        {
+          opacity: 0,
+        },
+        100
+      );
+    });
+});
